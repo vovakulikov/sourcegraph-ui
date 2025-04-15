918d3681f1237375dc94ca825a1bd839a1d00f54
Felix Kling <felix@felix-kling.de>
2025-04-15 19:02:07 +0200

    Add docs page

--- svelte.config.js
+++ svelte.config.js
@@ -3,7 +3,12 @@ import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 const config = {
-	preprocess: [vitePreprocess(), mdsvex()],
+	preprocess: [vitePreprocess(), mdsvex({
+		extensions: ['.svx'],
+		layout: {
+			_: './src/lib/layouts/MDXLayout.svelte'
+		}
+	})],
 	kit: { adapter: adapter() },
 	extensions: ['.svelte', '.svx']
 };
