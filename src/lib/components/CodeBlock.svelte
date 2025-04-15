0974d3bb1efc4e509f3ce965f525e3e69b5969b0
Vova Kulikov <vova@sourcegraph.com>
2025-04-15 19:04:26 +0200

    Add code blocks UI

---
+++ src/lib/components/CodeBlock.svelte
@@ -0,0 +1,29 @@
+<!--
+  @component
+  A component for displaying code examples with syntax highlighting.
+-->
+<script lang="ts">
+	export let code = '';
+	export let language = 'html';
+</script>
+
+<pre class="code-block" data-language={language}><code>{code}</code></pre>
+
+<style lang="scss">
+	.code-block {
+		margin: 0;
+		padding: 0.5rem;
+		background-color: var(--sg-sys-code-bg, #f8f8f8);
+		border-radius: 0.25rem;
+		overflow-x: auto;
+		font-family: monospace;
+		font-size: 0.9rem;
+		line-height: 1.5;
+		white-space: pre;
+		tab-size: 2;
+	}
+
+	code {
+		display: block;
+	}
+</style>
---
+++ src/lib/components/index.ts
@@ -0,0 +1,3 @@
+export { default as Icon } from './Icon.svelte';
+export { default as ComponentSandbox } from './ComponentSandbox.svelte';
+export { default as CodeBlock } from './CodeBlock.svelte';
