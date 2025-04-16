4afa6475c2c8e88f8b3195b9aa526b9752c611af
Jason Hawk Harris <jasonhawkharris@gmail.com>
2025-04-16 12:29:06 +0200

    Badge component updates

--- src/lib/components/index.ts
+++ src/lib/components/index.ts
@@ -1,3 +1,4 @@
 export { default as Icon } from './Icon.svelte';
+export { default as Badge, BADGE_VARIANTS } from './Badge.svelte';
 export { default as ComponentSandbox } from './ComponentSandbox.svelte';
 export { default as CodeBlock } from './CodeBlock.svelte';
--- src/routes/components/+layout.svelte
+++ src/routes/components/+layout.svelte
@@ -1,9 +1,10 @@
 <script lang="ts">
-		const COMPONENTS_PAGES = [
-			{ name: 'Icon', url: '/icon' }
-		]
+	const COMPONENTS_PAGES = [
+		{ name: 'Icon', url: '/icon' },
+		{ name: 'Badge', url: '/badge' }
+	];
 
-		let { children } = $props()
+	let { children } = $props();
 </script>
 
 <div class="root">
@@ -34,29 +35,29 @@
 	aside {
 		width: 300px;
 		height: 100%;
-    overflow: auto;
+		overflow: auto;
 		border-right: 2px solid var(--sg-sys-border-color);
 	}
 
-  ul {
-    list-style: none;
-    padding: 1rem 1.5rem;
-  }
+	ul {
+		list-style: none;
+		padding: 1rem 1.5rem;
+	}
 
-  .content {
+	.content {
 		height: 100%;
 		max-width: 700px;
 		flex-grow: 1;
 		padding: 1rem;
-    overflow: auto;
+		overflow: auto;
 
-    // Global styles override for component demo pages
-    :global(h2) {
-      margin-bottom: 2rem;
-    }
+		// Global styles override for component demo pages
+		:global(h2) {
+			margin-bottom: 2rem;
+		}
 
-    :global(p) {
-      margin-bottom: 1rem;
-    }
+		:global(p) {
+			margin-bottom: 1rem;
+		}
 	}
 </style>
